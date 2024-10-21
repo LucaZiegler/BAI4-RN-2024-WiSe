63a892000def8d86b5d27bdd8593b900333fa74c
Luca Ziegler <36471502+LucaZiegler@users.noreply.github.com>
2024-10-21 13:50:15 +0200

    Added WebService class

--- src/Praktikum1/Main.java
+++ src/Praktikum1/Main.java
@@ -1,10 +1,12 @@
 package Praktikum1;
 
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        IWebService webService = new WebService(8080);
+    public static void main(String[] args) throws IOException {
+        IWebService webService = new WebService(8080,3);
         webService.Run();
     }
 }
--- src/Praktikum1/WebService.java
+++ src/Praktikum1/WebService.java
@@ -1,29 +1,44 @@
 package Praktikum1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Semaphore;
 
 public class WebService implements IWebService {
+    public Semaphore _requestSemaphore;
     private final ServerSocket _serverSocket;
+    private boolean _serviceRunning = true;
 
-    public WebService(int port) throws IOException {
+    public WebService(int port, int maxThreads) throws IOException {
         _serverSocket = new ServerSocket(port);
+        _requestSemaphore = new Semaphore(maxThreads);
     }
 
     @java.lang.Override
     public void Run() {
-        while (true) {
+        while (_serviceRunning) {
             try {
+                _requestSemaphore.acquire();
                 Socket clientSocket = _serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            } catch (IOException e) {
+
+                String httpRequestRaw = "";
+                String line;
+                while (true) {
+                    line = readFromClient(in);
+                    httpRequestRaw += line;
+                }
+                //System.out.println(httpRequestRaw);
+            } catch (Exception e) {
                 System.out.println(e);
             }
         }
+    }
+
+    private String readFromClient(BufferedReader reader) throws IOException {
+        return reader.readLine();
     }
 }
