3e1dd44b47d4e376b184f9f6b70ea0fb3aa313cc
alexanderjakobs <alexander.jakobs@haw-hamburg.de>
2024-11-03 21:07:33 +0100

    Rückgabe der notwendigen Headers und korrekte Fehlerbehandlung,ausser 401 - soll noch teil b) mit Authorisierung gemacht werden

--- src/Praktikum1/WebServiceClientWorker.java
+++ src/Praktikum1/WebServiceClientWorker.java
@@ -20,10 +20,36 @@ public class WebServiceClientWorker extends Thread {
 
             String httpRequestRaw = "";
             String line;
+            String userAgent = null;
             while (!(line = readFromClient(in)).isEmpty()) {
                 httpRequestRaw += line + "\n";
+                if (line.startsWith("User-Agent")) {
+                    userAgent = line;
+                }
             }
-            System.out.println(httpRequestRaw);
+            if (httpRequestRaw.equals("")) {
+                System.out.println("HTTP/1.0 400 Bad Request");
+            }
+
+            //else if (!hasAuthorizationHeader(in)) {
+            //      System.out.println("HTTP/1.0 401 Unauthorized - Authorization required.");
+            //}
+            else if (httpRequestRaw.contains("/404.html")) {
+                System.out.println("HTTP/1.0 404 Not Found");
+            }
+
+            else if (userAgent != null && userAgent.contains("Firefox")) {
+                System.out.println("HTTP/1.0 200 OK\r\n");
+                System.out.println("Content-Type: text/plain\r\n");
+                System.out.println(("Content-Length: " + httpRequestRaw.length() + "\r\n"));
+                System.out.println("\r\n");
+            } else {
+                System.out.println("HTTP/1.0 406 Not Acceptable\r\n");
+                System.out.println("Content-Type: text/plain\r\n");
+                System.out.println("Content-Length: 0\r\n");
+                System.out.println("\r\n");
+            }
+
         } catch (Exception ex) {
 
         }finally {
