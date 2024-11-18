bc1e78fe310b806ba9e743101457ffd17510e448
Alisa Stoyanova <alisa.stoyanova@haw-hamburg.de>
2024-11-18 11:16:06 +0100

    Further implementation of the SMTP

--- src/praktikum_2/MailFile.java
+++ src/praktikum_2/MailFile.java
@@ -1,5 +1,3 @@
-package praktikum_2;
-
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
@@ -21,7 +19,7 @@ public class MailFile {
 
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
-        String domainLine = inFromServer.readLine();
+        String domainLine = receiveAndPrintInConsole(inFromServer);
         //System.out.println(domainLine);
         String[] domainLineParsed = domainLine.split(" "); // {code, domain, rest}
         if (Integer.parseInt(domainLineParsed[0]) != 220 || !domainLineParsed[1].equals(hostname)) {
@@ -29,7 +27,27 @@ public class MailFile {
             return;
         }
         String userAccount = config.getProperty("USER_ACCOUNT");
-        outToServer.writeBytes("EHLO localhost" + CRLF); // send status line
+        sendAndPrintInConsole(outToServer, "EHLO localhost"); // send status line
 
+        // The format for multiline replies requires that every line, except the last, begin with the reply code,
+        // followed immediately by a hyphen, "-" (also known as minus), followed by text. The last line will begin
+        // with the reply code, followed immediately by <SP>, optionally some text, and <CRLF>.
+        // https://datatracker.ietf.org/doc/html/rfc5321#section-4.2.1
+        String replyLine = receiveAndPrintInConsole(inFromServer);
+        while(replyLine.charAt(3) == '-') {
+            replyLine = receiveAndPrintInConsole(inFromServer);
+        }
+        sendAndPrintInConsole(outToServer, " "); // TODO: add AUTH
+    }
+
+    public static void sendAndPrintInConsole(DataOutputStream outToServer, String line) throws IOException {
+        outToServer.writeBytes(line + CRLF);
+        System.out.println("C: " + line);
+    }
+
+    public static String receiveAndPrintInConsole(BufferedReader inFromServer) throws IOException {
+        String line = inFromServer.readLine();
+        System.out.println("S: " + line);
+        return line;
     }
 }
